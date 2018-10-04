3a671e55f6e570b96dd9fc6591266ffbba056d01
Naman Verma <namanv3@Namans-MacBook-Pro.local>
2018-10-04 22:08:43 +0530

    Added Team Names

--- Initial_Scraping/scrap.py
+++ Initial_Scraping/scrap.py
@@ -9,27 +9,33 @@ names = []
 for player in players:
     pattern = re.compile(r"surname\":\".*?\"")
     surname = pattern.search(player)
+
     pattern = re.compile(r"forename\":\".*?\"")
     forename = pattern.search(player)
+
     pattern = re.compile(r"value\":\".*?\"")
     value = pattern.search(player).group()[8:-1]
+
+    pattern = re.compile(r"teamname\":\".*?\"")
+    team = pattern.search(player).group()[11:-1]
+
     if len(forename.group()) == 12:
-        names.append((surname.group()[10:-1],value))
+        names.append((surname.group()[10:-1],team,value))
         count += 1
     elif len(surname.group()) == 11:
-        names.append((forename.group()[11:-1],value))
+        names.append((forename.group()[11:-1],team,value))
         count += 1
     else:
-        names.append((forename.group()[11:-1] + " " + surname.group()[10:-1],value))
+        names.append((forename.group()[11:-1] + " " + surname.group()[10:-1],team,value))
         count += 1
 
 names.sort()
 with open('players.csv', 'w+') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-    filewriter.writerow(['Name', 'Value (in Millions)'])
+    filewriter.writerow(['Name', 'Team', 'Value (in Millions)'])
     for name in names:
-        filewriter.writerow([name[0], name[1]])
+        filewriter.writerow([name[0], name[1], name[2]])
 
 
 print(count)
