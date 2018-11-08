a0edf4d2482040ff690076e72f7a3b260adcd6e7
jatinjindal <jatinjindal369@gmail.com>
2018-11-09 03:18:21 +0530

    Leadboard code added

--- public_html/script.py
+++ public_html/script.py
@@ -2,9 +2,8 @@
 from sys import argv
 from ast import literal_eval
 import json 
-import tables
 json_decode = json.load(open('./app/js/static-data.json', 'r'))
-
+import unicodedata
 def data_by_id(forward, midfield, defender, goalkeeper):
      dforward = []
      dmidfield = []
@@ -13,25 +12,55 @@ def data_by_id(forward, midfield, defender, goalkeeper):
      for id in forward:
              for i in json_decode['elements']:
                      if i['id'] == id:
-                             dforward.append((id, i['first_name'], i['second_name'],i['total_points']))	
+                             dforward.append((id, i['first_name'].encode('ascii','ignore'), i['second_name'].encode('ascii','ignore'),i['total_points']))	
      for id in midfield:
              for i in json_decode['elements']:
                      if i['id'] == id:
-                             dmidfield.append((id, i['first_name'], i['second_name'],i['total_points']))
+                             dmidfield.append((id, i['first_name'].encode('ascii','ignore'), i['second_name'].encode('ascii','ignore'),i['total_points']))
      for id in defender:
              for i in json_decode['elements']:
                      if i['id'] == id:
-                             ddefender.append((id, i['first_name'], i['second_name'],i['total_points']))
+                             ddefender.append((id, i['first_name'].encode('ascii','ignore'), i['second_name'].encode('ascii','ignore'),i['total_points']))
      for id in goalkeeper:
              for i in json_decode['elements']:
                      if i['id'] == id:
-                             dgoalkeeper.append((id, i['first_name'], i['second_name'],i['total_points']))
+                             dgoalkeeper.append((id, i['first_name'].encode('ascii','ignore'), i['second_name'].encode('ascii','ignore'),i['total_points']))
      return (dforward, dmidfield, ddefender, dgoalkeeper)
 
-print(data_by_id(forw, mid,defe,gkp))
+
 
 forw = literal_eval(argv[1]);
 defe  = literal_eval(argv[2]);
 mid  = literal_eval(argv[3]);
 gkp  = literal_eval(argv[4]);
+temp = data_by_id(forw,mid,defe,gkp);
+
+
+print("<table style = 'min-width:75%;'>")
+print("<tr>")
+# print("<th> Player id </th>") 
+print("<th> First Name </th>") 
+print("<th> Last Name </th>") 
+print("<th> Total Points </th>") 
+print("<th> Position </th>") 
+print("</tr>")
+
+pos = ["F", "M" ,"D", "G"]
+for p in range(4):
+    for player in temp[p]:
+        print("<tr>")
+        flag =0 
+        for col in player:
+            # col = col.encode('ascii','ignore')
+            if(flag ==0):
+                flag =  1
+                continue
+            s = "<td> " + str(col) + " </td>";
+            print(s);
+        print("<td> " + pos[p] + " </td>");
+        print("</tr>")
+    
+
+print("</table>")
+
 
